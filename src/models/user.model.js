401f3cb29b1dc842398fe58bcaf8ec67f1c8421a
tintoll <belselios@gmail.com>
2019-04-30 07:39:52 +0900

    user table uuid 컬럼추가및 테스트(테스트중에러나중에 수정해야함)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,23 +1,39 @@
 'use strict'
+import { uuid } from "../utils/uuid";
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User',{
-    email : {
-      allowNull : false,
-      unique : true,
-      type : DataTypes.STRING,
-      validate : {
-        isEmail : true,
+  const User = sequelize.define(
+    "User",
+    {
+      uuid: {
+        allowNull: false,
+        unique: true,
+        type: "BINARY(16)",
+        // defaultValue 로 uuid() 로 부터 생성된 hex 문자열을 Byte Buffer 로 변환시켜서 넣는 걸 확인하실 수 있습니다. 
+        // 이는 곧 mysql 에서 unhex() 하는 것과 같은 결과를 낳습니다.
+        defaultValue: () => Buffer(uuid(), "hex"),
+        get: function() {
+          return Buffer.from(this.getDataValue("uuid").toString("hex"));
+        }
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        alloNull: false,
+        type: DataTypes.STRING
       }
     },
-    password : {
-      alloNull : false,
-      type : DataTypes.STRING
+    {
+      tableNames: "users",
+      timestamps: true
     }
-  }, {
-      tableNames: 'users',
-      timestamps: true,
-  });
+  );
 
   User.associate = function (models) {
     // associations
--- src/tests/functions/uuid.test.js
+++ src/tests/functions/uuid.test.js
@@ -1,6 +1,18 @@
 import { uuid } from "../../utils/uuid";
+import randomString from "random-string";
+import { models } from "../../models";
 
 test('ordered UUID가 출력되어야 합니다.', () => {
   const orderedUuid = uuid();
   expect(orderedUuid).toMatch(/\b4[0-9A-Fa-f]{31}\b/g);
-})
+});
+
+test('사용자를 생성하면 uuid가 정상 생성되어야 합니다.', async () => {
+  const user = await models.User.create({
+    email : `${randomString()}@test.com`,
+    password : randomString()
+  });
+
+  expect(user.uuid).toMatch(/\b4[0-9A-Fa-f]{31}\b/g);
+
+});
